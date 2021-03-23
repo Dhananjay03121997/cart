537d8d6398c58ac957abc3e011497366fb11f782
unknown <dhananjay.ivision@gmail.com>
2021-03-23 21:43:52 +0530

    Setup react

---
+++ src/App.js
@@ -0,0 +1,46 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div className="background">
+      <div>
+          <table cellPadding="10">
+            <tbody>
+              <tr>
+                <td>
+                  <h2>Products</h2>
+                </td>
+              </tr>
+              <tr>
+                <td>Bread</td>
+                <td>Price</td>
+                <td><button>Add</button></td>
+              </tr>
+            </tbody>
+          </table>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
