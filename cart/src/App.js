2a0f78e967496a6ffe6378797c041635924af532
unknown <dhananjay.ivision@gmail.com>
2021-03-24 19:02:46 +0530

    Added Bootstrap

--- cart/src/App.js
+++ cart/src/App.js
@@ -1,15 +1,40 @@
-// import logo from './logo.svg';
 import './App.css';
 import data from './data/data.json';
-let data1;
+import setList from './data/cart.json';
+import 'bootstrap/dist/css/bootstrap.css';
 function Product() {
   return (
-    data1 = data.product.map((product, index)=>{
-     return <tr key={index} className="tr-border"><td>{product.name}</td><td>${product.price}</td> <td><button className="btnadd">Add</button></td> </tr>
-    })
+                  <table className="table" cellPadding="10" cellSpacing="10">
+                    <thead>
+                      <tr><th colSpan="2">Products</th></tr>
+                      <tr>
+                        <th scope="col" className = "w-100">Name</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Operation</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    {data.product.map((product, index)=>{
+                      return <tr key={index}><td>{product.name}</td><td>${product.price}</td><td><button className="btn btn-primary" value={index} onClick={addToCart}>Add</button></td></tr>
+                    })
+                  }
+                    </tbody>
+                  </table>
   );
 }
+function Cart(){
+  return(
+     setList.cart.map((item)=>{
+     return <tr><td>{item.name}</td></tr>
+    })
+  )
+}
+
+const addToCart = (i) => {
+  console.log(data.product[i.target.value]);
+}
+
 
 // function 
 
-export default Product;
+export {Product, Cart };
