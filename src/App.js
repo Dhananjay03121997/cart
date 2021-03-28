119b24dbfda61063ee619159bedfc6e6692d5423
unknown <dhananjay.ivision@gmail.com>
2021-03-28 12:21:37 +0530

    Added total value count

--- src/App.js
+++ src/App.js
@@ -6,20 +6,9 @@ import axios from 'axios'
 
 let data = products.product;
 function Product() {
-    // const [data, setProductData] = useState([]);
-
-    // useEffect(() => {
-    //   loadProducts();
-    // }, []);
-  
-    // const loadProducts = async () => {
-    //   const result = await axios.get("http://localhost:3003/product");
-    //   setProductData(result.data);
-    // };      NOT WORKING AFTER PUSHED ON HEROKU APP
-
-  let arr = [];
+  let arr = [], totalVal = 0;
   const [cart, setCart] = useState([]);
-  const [quantity, setQuantity] = useState([]);
+  const [total, setTotal] = useState(0);
   function handleClick(e) {
     e.target.disabled = true
     let i = e.target.attributes[1].value;
@@ -28,7 +17,7 @@ function Product() {
       price: data[i].price,
       quantity: data[i].quantity,
       requirement: 1,
-      item_total: data[i].price * 1
+      item_total: data[i].price * 1,
     }
     if (data[i].name == "cheese" || data[i].name == "Cheese") {
       product.requirement = 2;
@@ -36,9 +25,18 @@ function Product() {
     }
     arr.push(product);
     setCart(current=>[...current, arr]);
+}
+
+useEffect(() => {
+  countTotal();
+}, [cart]);
+
+  const countTotal=()=>{
+    for (let i = 0; i < cart.length; i++) {
+      totalVal += cart[i][0].item_total;
+    }
+    setTotal(totalVal);
   }
-
-
     function onClick(e){
       let i = e.target.value, add = 1, count = 1;
       cart[i][0].name == "Cheese" ? add = 2 : add = 1;
@@ -48,10 +46,14 @@ function Product() {
       cart[i][0].item_total = cart[i][0].price * cart[i][0].requirement;
   }
   if(e.target.name == "less"){
-      cart[i][0].requirement = cart[i][0].requirement - 1;
+      cart[i][0].requirement = cart[i][0].requirement - add;
       cart[i][0].item_total = cart[i][0].requirement * cart[i][0].price;
   }
-    setQuantity(current =>[...current , cart[i][0].requirement]);
+  let totalVal = 0;
+  for (let i = 0; i < cart.length; i++) {
+    totalVal += cart[i][0].item_total;
+  }
+  setTotal(totalVal);
     }
   return (
     <div className="container">
@@ -76,8 +78,7 @@ function Product() {
                           <td>{product.name}</td>
                           <td>${product.price}</td>
                           <td>
-                            <button className="btn btn-primary" value={index} key={index} onClick={handleClick} 
-                              >
+                            <button className="btn btn-primary" value={index} key={index} onClick={handleClick}>
                               Add
                             </button></td>
                           </tr>}
@@ -104,21 +105,26 @@ function Product() {
                           <th >Price</th>
                           <th >Operation</th>
                         </tr>
+                      </thead>
+                      <tbody>
                           {cart.map((item, index)=>{
                             return <tr key={index}>
                               <td >{item[0].name}</td>
                               <td >${item[0].price}</td>
-                              <td className = "col-lg-4 d-inline">
+                              <tr>
+                              <td className = "col-lg-4 ">
                                 <button name="less" className="btn-outline-primary btn-xs" value={index} onClick={onClick} disabled={item[0].requirement == 1}>-</button>
                                 <input type="text" style={{width:"30px"}} value={item[0].requirement}/>
                                 <button name="add" className="btn-xs btn-outline-secondary add" value={index} onClick={onClick} disabled={item[0].requirement == item[0].quantity}>+</button>
                                 </td>
-                                <tr>item price {item[0].price} * {item[0].requirement} = {item[0].item_total.toFixed(2)}</tr>
+                                </tr>
+                                <tr className="text-right">item price {item[0].price} * {item[0].requirement} = {item[0].item_total.toFixed(2)}</tr>
                                 </tr>
                           })
                           }
-                      </thead>
-                      <tbody>
+                           <tr className="font-weight-bolder"><td></td><td >Total</td><td>{total == 0 ? total : total.toFixed(2)}</td></tr>
+                          
+                            
                       </tbody>
                     </table>
                     </div>
